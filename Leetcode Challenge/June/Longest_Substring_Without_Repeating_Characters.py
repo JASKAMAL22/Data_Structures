b16a547d5bf8b3ebb2ba799d28e4b74659362346
JASKAMAL SINGH <54163864+JASKAMAL22@users.noreply.github.com>
2022-06-20 17:07:08 +0530

    Create Longest_Substring_Without_Repeating_Characters.py

---
+++ Leetcode Challenge/June/Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,38 @@
+'''
+STATEMENT:
+Given a string s, find the length of the longest substring without repeating characters.
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+--------------------------------------------------------------
+'''
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        start = maxLength = 0
+        usedChar = {}
+        
+        for i in range(len(s)):
+            if s[i] in usedChar and start <= usedChar[s[i]]:
+                start = usedChar[s[i]] + 1
+            else:
+                maxLength = max(maxLength, i - start + 1)
+
+            usedChar[s[i]] = i
+
+        return maxLength
+      
+'''
+--------------------------------------------------------------
+Constraints:
+
+0 <= s.length <= 5 * 104
+s consists of English letters, digits, symbols and spaces.
+'''
