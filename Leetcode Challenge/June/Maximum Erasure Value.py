c419f098a4871cb655abb5acc642f83f7cadb1af
JASKAMAL SINGH <54163864+JASKAMAL22@users.noreply.github.com>
2022-06-23 23:53:38 +0530

    Create Maximum Erasure Value.py

---
+++ Leetcode Challenge/June/Maximum Erasure Value.py
@@ -0,0 +1,40 @@
+'''
+STATEMENT:
+You are given an array of positive integers nums and want to erase a subarray containing unique elements. The score you get by erasing the subarray is equal to the sum of its elements.
+
+Return the maximum score you can get by erasing exactly one subarray.
+
+An array b is called to be a subarray of a if it forms a contiguous subsequence of a, that is, if it is equal to a[l],a[l+1],...,a[r] for some (l,r).
+-----------------------------------------------------------
+'''
+class Solution:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        beg, end, S, n, sm = 0, 0, set(), len(nums), 0
+        ans = 0
+        while end < n:
+            if nums[end] not in S:
+                sm += nums[end]
+                S.add(nums[end])
+                end += 1
+                ans = max(ans, sm)
+            else:
+                sm -= nums[beg]
+                S.remove(nums[beg])
+                beg += 1
+        
+        return ans 
+     
+    
+'''
+Example 1:
+
+Input: nums = [4,2,4,5,6]
+Output: 17
+Explanation: The optimal subarray here is [2,4,5,6].
+----------------------------------------------
+Example 2:
+
+Input: nums = [5,2,1,2,5,2,1,2,5]
+Output: 8
+Explanation: The optimal subarray here is [5,2,1] or [1,2,5].
+'''
